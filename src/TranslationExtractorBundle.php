59278337b6ccfd7d3c9e7575eb3d3441460993d3
Daniel Richardt <d.richardt@ottcomputer.de>
2019-11-22 15:55:07 +0100

    Initial commit

---
+++ src/TranslationExtractorBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * (c) Daniel Richardt <@gmail.com>.
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace dpmR\Bundle\TranslationExtractorBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+final class TranslationExtractorBundle extends Bundle
+{
+}
